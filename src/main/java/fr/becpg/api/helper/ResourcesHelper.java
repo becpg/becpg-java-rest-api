7bf6dadc107b79091cbd675a5b1e66ce494fb20f
fredboxebeld <frederic.boxebeld@becpg.fr>
2023-11-15 17:42:47 +0100

    Fix #19343 - [Feature] Add helper class for resources

---
+++ src/main/java/fr/becpg/api/helper/ResourcesHelper.java
@@ -0,0 +1,28 @@
+package fr.becpg.api.helper;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.nio.charset.StandardCharsets;
+
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Service;
+import org.springframework.util.FileCopyUtils;
+
+/**
+ * Utility class to manage {@link Resource}
+ */
+@Service
+public class ResourcesHelper {
+
+	/**
+	 * Converts a {@link Resource} to a String value
+	 * @param resource
+	 * @return
+	 * @throws IOException 
+	 */
+	public static String asString(Resource resource) throws IOException {
+		Reader reader = new InputStreamReader(resource.getInputStream(), StandardCharsets.UTF_8);
+		return FileCopyUtils.copyToString(reader);
+	}
+}
